c27660a3c5f64785c1dcbe1fb82dd4e1bb3f5ed5
Jan Kirchner <jan.k.kirchner@gmail.com>
2020-03-21 21:01:07 +0100

    work on business detail page

--- src/components/Businesses/Business.js
+++ src/components/Businesses/Business.js
@@ -1,38 +1,75 @@
 import React from "react";
-import { Box, Icon, List, ListItem, ListItemIcon } from "@material-ui/core";
+import {
+  Box,
+  Icon,
+  List,
+  ListItem,
+  ListItemIcon,
+  IconButton
+} from "@material-ui/core";
 import { getBusinessById } from "../../services/business.service";
+import styled from "styled-components";
 
 const BusinessComponent = ({
   match: {
     params: { id }
-  }
+  },
+  history: { goBack }
 }) => {
   const business = getBusinessById(id);
-  if (!business) return <span>Laden konnte nicht gefunden werden</span>;
   return (
-    <Box>
-      <p>
-        <Icon>{business.icon}</Icon>
-      </p>
-      <h1>{business.title}</h1>
-
-      <hr />
-      <List>
-        <ListItem button>
-          <ListItemIcon>
-            <Icon>euro</Icon>
-          </ListItemIcon>
-          <h2>Bezahlen</h2>
-        </ListItem>
-        <ListItem>
-          <ListItemIcon>
-            <Icon>money</Icon>
-          </ListItemIcon>
-          <h2>Gutschein kaufen</h2>
-        </ListItem>
-      </List>
-    </Box>
+    <>
+      <FlexBox>
+        <SpaceContainer>
+          <IconButton onClick={goBack} color="inherit">
+            <Icon>navigate_before</Icon>
+          </IconButton>
+        </SpaceContainer>
+        <h1>
+          {business ? (
+            <>
+              <Icon>{business.icon}</Icon> {business.title}
+            </>
+          ) : (
+            "Nicht gefunden"
+          )}
+        </h1>
+        <SpaceContainer />
+      </FlexBox>
+      {business ? (
+        <Box>
+          <List>
+            <ListItem button>
+              <ListItemIcon>
+                <Icon>euro</Icon>
+              </ListItemIcon>
+              <h2>Bezahlen</h2>
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <Icon>money</Icon>
+              </ListItemIcon>
+              <h2>Gutschein kaufen</h2>
+            </ListItem>
+          </List>
+        </Box>
+      ) : (
+        <span>Laden konnte nicht gefunden werden</span>
+      )}
+    </>
   );
 };
+
+const FlexBox = styled(Box)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SpaceContainer = styled(Box)`
+  display: flex;
+  flex-grow: 1;
+  justify-content: flex-start;
+`;
 
 export default BusinessComponent;
