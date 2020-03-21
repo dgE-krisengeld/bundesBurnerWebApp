8c25106f31953bec61732147aee6d14311ded469
Jan Kirchner <jan.k.kirchner@gmail.com>
2020-03-21 19:15:18 +0100

    update list of mock businesses

--- src/dummyData/markers.js
+++ src/dummyData/markers.js
@@ -1,29 +1,42 @@
 export const markers = {
   biz1: {
     coordinates: {
-      latitude: 50.014123,
-      longitude: 8.263218,
+      latitude: 49.8804713,
+      longitude: 8.6592032
     },
-    title: 'Business 1',
-    id: 'biz1',
-    icon: 'ice-cream',
+    title: "Pizza Mono",
+    icon: "local_pizza"
   },
   biz2: {
     coordinates: {
-      latitude: 50.012123,
-      longitude: 8.264218,
+      latitude: 49.879783,
+      longitude: 8.6567384
     },
-    title: 'Business 2',
-    id: 'biz2',
-    icon: 'hamburger',
+    title: "Baobab",
+    icon: "local_dining"
   },
   biz3: {
     coordinates: {
-      latitude: 50.013123,
-      longitude: 8.262218,
+      latitude: 49.8774271,
+      longitude: 8.6459071
     },
-    title: 'Business 3',
-    id: 'biz3',
-    icon: 'bread-slice',
+    title: "Timm's Café",
+    icon: "local_cafe"
   },
+  biz4: {
+    coordinates: {
+      latitude: 49.872911,
+      longitude: 8.659336
+    },
+    title: "Sausalitos",
+    icon: "local_bar"
+  },
+  biz5: {
+    coordinates: {
+      latitude: 49.8730224,
+      longitude: 8.6511233
+    },
+    title: "Best Worscht in Town",
+    icon: "fastfood"
+  }
 };
