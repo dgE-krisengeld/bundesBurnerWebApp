9cac147b165aaf8283c5be19e37af74e390aab1a
Jan Kirchner <jan.k.kirchner@gmail.com>
2020-03-21 19:22:23 +0100

    list mock businesses

--- src/components/Businesses/Businesses.js
+++ src/components/Businesses/Businesses.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import {Icon} from '../Utils/Icon';
+import React from "react";
+import { Box, List, ListItem, Icon } from "@material-ui/core";
+import { markers } from "../../dummyData/markers";
 
 export function BusinessesComponent() {
-
   return (
-    <div>Businesses</div>
+    <Box>
+      <h1>Lokale Geschäfte</h1>
+      <List>
+        {Object.entries(markers).map(([key, { coordinates, title, icon }]) => (
+          <ListItem>
+            <h3>
+              <Icon>{icon}</Icon> {title}
+            </h3>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
   );
 }
