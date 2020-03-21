7c311f6adb1bc2e136cbd4dcb28e152487ceb4be
Jan Kirchner <jan.k.kirchner@gmail.com>
2020-03-21 20:25:52 +0100

    create business detail page

--- src/components/Businesses/Business.js
+++ src/components/Businesses/Business.js
@@ -1,32 +1,38 @@
-import React from 'react';
-import {View, Text, Button, Alert} from 'react-native';
-import MapView, {Marker} from 'react-native-maps';
-import {Icon} from '../Utils/Icon';
+import React from "react";
+import { Box, Icon, List, ListItem, ListItemIcon } from "@material-ui/core";
+import { getBusinessById } from "../../services/business.service";
 
-export function BusinessComponent({marker}) {
+const BusinessComponent = ({
+  match: {
+    params: { id }
+  }
+}) => {
+  const business = getBusinessById(id);
+  if (!business) return <span>Laden konnte nicht gefunden werden</span>;
   return (
-    <View style={{flex: 1}}>
-      <MapView
-        style={{height: 150}}
-        initialRegion={{
-          latitude: 50.014123,
-          longitude: 8.263218,
-          latitudeDelta: 0.012,
-          longitudeDelta: 0.011,
-        }}>
-        <Marker
-          title={marker.title}
-          key={marker.id}
-          coordinate={marker.coordinates}>
-          <Icon name={marker.icon} style={{color: 'red', fontSize: 22}} />
-        </Marker>
-      </MapView>
-        <View style={{margin: 20}}>
-            <Text> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, animi assumenda aut cum debitis dolores eius esse est laborum maiores modi perferendis provident qui ratione sequi tempora ut vel voluptates.</Text>
-        </View>
-        <View>
-            <Button title={"Bezahlen"} onPress={() => Alert.alert("Pay now")}/>
-        </View>
-    </View>
+    <Box>
+      <p>
+        <Icon>{business.icon}</Icon>
+      </p>
+      <h1>{business.title}</h1>
+
+      <hr />
+      <List>
+        <ListItem button>
+          <ListItemIcon>
+            <Icon>euro</Icon>
+          </ListItemIcon>
+          <h2>Bezahlen</h2>
+        </ListItem>
+        <ListItem>
+          <ListItemIcon>
+            <Icon>money</Icon>
+          </ListItemIcon>
+          <h2>Gutschein kaufen</h2>
+        </ListItem>
+      </List>
+    </Box>
   );
-}
+};
+
+export default BusinessComponent;
