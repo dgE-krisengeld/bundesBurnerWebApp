52170482c9afd42d5faf937cf16ae48b0febfb86
Jan Kirchner <jan.k.kirchner@gmail.com>
2020-03-21 17:25:17 +0100

    remove unused Link import

--- src/navigation/NavigationContainer.js
+++ src/navigation/NavigationContainer.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Container from "@material-ui/core/Container";
 import { HomeContainer } from "../containers/Home/Home";
 import { BusinessesContainer } from "../containers/Businesses/Businesses";
